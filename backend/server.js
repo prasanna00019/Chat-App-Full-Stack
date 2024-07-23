69ea8b8dfe45dd7b8d353e6d1895c89731562afe
prasanna.000019@gmail.com <prasanna.000019@gmail.com>
2024-07-23 13:00:34 +0530

    DEPLOYMENT CHANGES

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,23 @@ import authRoutes from './routes/AuthRoutes.js'
 import messageRoutes from './routes/MessageRoutes.js'
 import userRoutes from './routes/UserRoutes.js'
 ;import connectDB from "./db/connectDB.js";
+import path from "path";
 import { app, server } from "./socket/socket.js";
 const PORT=process.env.PORT || 5000
+const __dirname=path.resolve()
 app.use(express.json());
 app.use(cookieParser());
-app.get('/',(req,res)=>{
-    res.send("hello world sever 5000")
-})
+// app.get('/',(req,res)=>{
+//     res.send("hello world sever 5000")
+// })
 //middleware
 app.use('/api/auth',authRoutes); 
 app.use('/api/messages',messageRoutes);
 app.use('/api/users',userRoutes);
+app.use(express.static(path.join(__dirname,"frontend/dist")))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 server.listen(PORT,()=>{
     console.log(`hello ${PORT}`);
     connectDB();
